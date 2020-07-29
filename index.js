6404008cfcf722d1a90062bea5a86d3f85856f93
Samiha Nanjiba <snanjiba@gmail.com>
2020-07-29 16:07:17 +0600

    Added logic for registering user in the database

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-require('./services/passport');
 require('./models/User');
+require('./services/passport');
+
 // const authRoutes = require('./routes/authRoutes');
 
 mongoose.connect(keys.mongoURI);
@@ -15,9 +16,8 @@ require('./routes/authRoutes')(app);
 // in the folder routes
 
 /*test route, don't need anymore
-prints the object below in the browser
-when visited localhost:5000*/
-// //route handler
+prints the object below in the browser when visited localhost:5000*/
+// route handler
 // app.get('/', (req, res) => {
 //     res.send({bye: 'blah'});
 // });
--- services/passport.js
+++ services/passport.js
@@ -1,15 +1,32 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token',accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
-    })
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            
+            User.findOne({ googleID: profile.id })
+                .then((existingUser) => {
+                    if(existingUser) {
+                        // We already have a record of the given profile ID
+                    }else{
+                        // We need to make a new record with the given profile ID
+                        new User ({googleID: profile.id}).save();
+                    }
+                })
+        
+            // console.log('access token',accessToken);
+            // console.log('refresh token', refreshToken);
+            // console.log('profile', profile);
+        }
+    )
 );
